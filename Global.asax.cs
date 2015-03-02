23d4e49e5614c3029fde9f6cc6c2b9aed12e101a
Ahmed ElSayed <ahmels@microsoft.com>
2015-03-02 14:00:36 -0800

    add new favicon and set Thread Principal to user principal

--- Global.asax.cs
+++ Global.asax.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Security.Principal;
 using System.Web;
 using System.Web.Http;
 using System.Web.Security;
@@ -17,6 +18,17 @@ namespace ARMExplorer
         {
             ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
             WebApiConfig.Register(GlobalConfiguration.Configuration);
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+            var displayName = HttpContext.Current.Request.Headers["X-MS-CLIENT-DISPLAY-NAME"];
+            var principalName = HttpContext.Current.Request.Headers["X-MS-CLIENT-PRINCIPAL-NAME"];
+            if (!string.IsNullOrWhiteSpace(principalName) ||
+                !string.IsNullOrWhiteSpace(displayName))
+            {
+                HttpContext.Current.User = new GenericPrincipal(new GenericIdentity(principalName ?? displayName), new [] {"User"});
+            }
         }
     }
 }
