2009d5e9db606dff0f7f9605bbf0e0ee40bfe46b
Hosam Shobak <hosam.shobak@gmail.com>
2015-12-02 11:34:48 -0800

    moving the swap slots script to use Azure Powershel 1.X

--- Tools/SwapWithStagingSlots.ps1
+++ Tools/SwapWithStagingSlots.ps1
@@ -1,22 +1,22 @@
 ﻿# make sure the script stops if one of the swaps failed
 $ErrorActionPreference = "Stop"
 
-Select-AzureSubscription -SubscriptionName "Websites migration" -Current
+Select-AzureRmSubscription -SubscriptionName "Websites migration"
 
 $ParametersObject = @{
 	targetSlot = "staging"
 }
 
 Write-Host swapping bay site ....
-Invoke-AzureResourceAction -ResourceGroupName Default-Web-MSFTWestUS -ResourceType Microsoft.Web/sites -ResourceName explorer-bay -Action slotsswap -Parameters $ParametersObject -ApiVersion 2015-08-01 -Force
+Invoke-AzureRmResourceAction -ResourceGroupName Default-Web-MSFTWestUS -ResourceType Microsoft.Web/sites -ResourceName explorer-bay -Action slotsswap -Parameters $ParametersObject -ApiVersion 2015-08-01 -Force
 
 Write-Host swapping blu site ....
-Invoke-AzureResourceAction -ResourceGroupName Default-Web-MSFTEastUS -ResourceType Microsoft.Web/sites -ResourceName explorer-blu -Action slotsswap -Parameters $ParametersObject -ApiVersion 2015-08-01 -Force
+Invoke-AzureRmResourceAction -ResourceGroupName Default-Web-MSFTEastUS -ResourceType Microsoft.Web/sites -ResourceName explorer-blu -Action slotsswap -Parameters $ParametersObject -ApiVersion 2015-08-01 -Force
 
 Write-Host swapping db3 site ....
-Invoke-AzureResourceAction -ResourceGroupName Default-Web-MSFTNorthEurope -ResourceType Microsoft.Web/sites -ResourceName explorer-db3 -Action slotsswap -Parameters $ParametersObject -ApiVersion 2015-08-01 -Force
+Invoke-AzureRmResourceAction -ResourceGroupName Default-Web-MSFTNorthEurope -ResourceType Microsoft.Web/sites -ResourceName explorer-db3 -Action slotsswap -Parameters $ParametersObject -ApiVersion 2015-08-01 -Force
 
 Write-Host swapping hk1 site ....
-Invoke-AzureResourceAction -ResourceGroupName Default-Web-MSFTEastAsia -ResourceType Microsoft.Web/sites -ResourceName explorer-hk1 -Action slotsswap -Parameters $ParametersObject -ApiVersion 2015-08-01 -Force
+Invoke-AzureRmResourceAction -ResourceGroupName Default-Web-MSFTEastAsia -ResourceType Microsoft.Web/sites -ResourceName explorer-hk1 -Action slotsswap -Parameters $ParametersObject -ApiVersion 2015-08-01 -Force
 
 Write-Host Done Swapping all sites
