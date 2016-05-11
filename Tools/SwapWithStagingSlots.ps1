0a9fb628ccdd5f7f6e047b169bc0c5ea4b12c4ea
davidebbo <david.ebbo@microsoft.com>
2016-05-11 16:38:50 -0700

    Fix resource group names in swap ps1 file

--- Tools/SwapWithStagingSlots.ps1
+++ Tools/SwapWithStagingSlots.ps1
@@ -8,15 +8,15 @@ $ParametersObject = @{
 }
 
 Write-Host swapping bay site ....
-Invoke-AzureRmResourceAction -ResourceGroupName Default-Web-MSFTWestUS -ResourceType Microsoft.Web/sites -ResourceName explorer-bay -Action slotsswap -Parameters $ParametersObject -ApiVersion 2015-08-01 -Force
+Invoke-AzureRmResourceAction -ResourceGroupName explorer-bay -ResourceType Microsoft.Web/sites -ResourceName explorer-bay -Action slotsswap -Parameters $ParametersObject -ApiVersion 2015-08-01 -Force
 
 Write-Host swapping blu site ....
-Invoke-AzureRmResourceAction -ResourceGroupName Default-Web-MSFTEastUS -ResourceType Microsoft.Web/sites -ResourceName explorer-blu -Action slotsswap -Parameters $ParametersObject -ApiVersion 2015-08-01 -Force
+Invoke-AzureRmResourceAction -ResourceGroupName explorer-blu -ResourceType Microsoft.Web/sites -ResourceName explorer-blu -Action slotsswap -Parameters $ParametersObject -ApiVersion 2015-08-01 -Force
 
 Write-Host swapping db3 site ....
-Invoke-AzureRmResourceAction -ResourceGroupName Default-Web-MSFTNorthEurope -ResourceType Microsoft.Web/sites -ResourceName explorer-db3 -Action slotsswap -Parameters $ParametersObject -ApiVersion 2015-08-01 -Force
+Invoke-AzureRmResourceAction -ResourceGroupName explorer-db3 -ResourceType Microsoft.Web/sites -ResourceName explorer-db3 -Action slotsswap -Parameters $ParametersObject -ApiVersion 2015-08-01 -Force
 
 Write-Host swapping hk1 site ....
-Invoke-AzureRmResourceAction -ResourceGroupName Default-Web-MSFTEastAsia -ResourceType Microsoft.Web/sites -ResourceName explorer-hk1 -Action slotsswap -Parameters $ParametersObject -ApiVersion 2015-08-01 -Force
+Invoke-AzureRmResourceAction -ResourceGroupName explorer-hk1 -ResourceType Microsoft.Web/sites -ResourceName explorer-hk1 -Action slotsswap -Parameters $ParametersObject -ApiVersion 2015-08-01 -Force
 
 Write-Host Done Swapping all sites
