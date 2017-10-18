72992910604423e9de0cd955fa0cfd3969552680
bala16 <balag@outlook.com>
2017-10-17 19:31:47 -0700

    Use skiptoken to retrieve complete list of resources  Currently all calls to ARM APIs ignore skiptoken link. This meant only partial list of resources would be retrieved in some cases. This change adds support to walk the skiptoken url when retrieving  GET /subscriptions/{subscriptionId}/resources. Note: Other API calls still have this limitation. Resource Search uses the same API but has not been updated to make use of this new functionality yet (only looks at top 100 resources in a subscription)

---
+++ Model/ArmResourceListResult.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.ObjectModel;
+
+namespace ARMExplorer.Model
+{
+    public class ArmResourceListResult
+    {
+        [Newtonsoft.Json.JsonProperty("value", Required = Newtonsoft.Json.Required.Default, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
+        public Collection<ArmResource> Value { get; set; }
+
+        [Newtonsoft.Json.JsonProperty("nextLink", Required = Newtonsoft.Json.Required.Default, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
+        public string NextLink { get; set; }
+    }
+}
