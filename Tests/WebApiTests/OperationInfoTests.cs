6c94a43b2a7de7648e984f666203d88fa8e7f4d6
Bala G <balag@outlook.com>
2021-08-25 17:59:32 -0700

    Add ARM Host validation tests

---
+++ Tests/WebApiTests/OperationInfoTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ARMExplorer.Controllers;
+using Xunit;
+
+namespace Tests.WebApiTests
+{
+    public class OperationInfoTests
+    {
+        [Theory]
+        [InlineData("", false)]
+        [InlineData(null, false)]
+        [InlineData("https://management.azure.com/subscriptions", true)]
+        [InlineData("https://management.azurre.com/subscriptions", false)]
+        [InlineData("https://abcd.com", false)]
+        public void IsValidOperationInfoHost(string url, bool isValidHost)
+        {
+            var operationInfo = new OperationInfo() {Url = url};
+            Assert.Equal(isValidHost, operationInfo.IsValidHost());
+        }
+    }
+}
